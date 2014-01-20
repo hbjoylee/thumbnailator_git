c62aaf30e18814dd968990a1d4ffcf83d48fe174
JamesLi <hbjoylee@gmail.com>
2014-01-20 14:59:50 +0800

    text water mark

--- TextWatermark.java
+++ TextWatermark.java
@@ -1,94 +1,125 @@
 package net.coobird.thumbnailator.filters;
 
 import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 
-import net.coobird.thumbnailator.builders.BufferedImageBuilder;
 import net.coobird.thumbnailator.geometry.Position;
+import net.coobird.thumbnailator.util.BufferedImages;
 
 /**
- * This class applies a watermark to an image.
+ * An {@link ImageFilter} which will overlay a text caption to an image.
  * 
  * @author coobird
- * 
+ *
  */
-public class TextWatermark implements ImageFilter {
-  /**
-   * The position of the watermark.
-   */
-  private final Position position;
+public class TextWatermark implements ImageFilter
+{
+	/**
+	 * The text of the caption.
+	 */
+	private final String caption;
+	
+	/**
+	 * The font of text to add.
+	 */
+	private final Font font;
+	
+	/**
+	 * The color of the text to add.
+	 */
+	private final Color c;
+	
+	/**
+	 * The opacity level of the text to add.
+	 * <p>
+	 * The value should be between {@code 0.0f} to {@code 1.0f}, where
+	 * {@code 0.0f} is completely transparent, and {@code 1.0f} is completely
+	 * opaque.
+	 */
+	private final float alpha;
+	
+	/**
+	 * The position at which the text should be drawn.
+	 */
+	private final Position position;
+	
+	
+	/**
+	 * Instantiates a filter which adds a text caption to an image.
+	 * 
+	 * @param caption	The text of the caption.
+	 * @param font		The font of the caption.
+	 * @param c			The color of the caption.
+	 * @param alpha		The opacity level of caption.
+	 * 					<p>
+	 * 					The value should be between {@code 0.0f} and
+	 * 					{@code 1.0f}, where {@code 0.0f} is completely
+	 * 					transparent, and {@code 1.0f} is completely opaque.
+	 * @param position	The position of the caption.
+	 */
+	public TextWatermark(String caption, Font font, Color c, float alpha,
+			Position position)
+	{
+		this.caption = caption;
+		this.font = font;
+		this.c = c;
+		this.alpha = alpha;
+		this.position = position;
+	}
 
-  /**
-   * The watermark image.
-   */
-  private final WaterText WaterText;
+	/**
+	 * Instantiates a filter which adds a text caption to an image.
+	 * <p>
+	 * The opacity of the caption will be 100% opaque.
+	 * 
+	 * @param caption	The text of the caption.
+	 * @param font		The font of the caption.
+	 * @param c			The color of the caption.
+	 * @param position	The position of the caption.
+	 */
+	public TextWatermark(String caption, Font font, Color c, Position position)
+	{
+		this.caption = caption;
+		this.font = font;
+		this.c = c;
+		this.alpha = 1.0f;
+		this.position = position;
+	}
 
-  /**
-   * The opacity of the watermark.
-   */
-  private final float opacity;
+	public BufferedImage apply(BufferedImage img)
+	{
+		BufferedImage newImage = BufferedImages.copy(img);
+		
+		Graphics2D g = newImage.createGraphics();
+		g.setFont(font);
+		g.setColor(c);
+		g.setComposite(
+				AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha)
+		);
+		
+		int imageWidth = img.getWidth();
+		int imageHeight = img.getHeight();
+		
+		int captionWidth = g.getFontMetrics().stringWidth(caption);
+		int captionHeight = g.getFontMetrics().getHeight() / 2;
+		
+		Point p = position.calculate(
+				imageWidth,	imageHeight, captionWidth, 0,
+				position.getinsetLeft(), position.getinsetRight(), 
+				position.getinsetTop(), position.getinsetBottom()
+		);
 
-  /**
-   * Instantiates a filter which applies a watermark to an image.
-   * 
-   * @param position
-   *          The position of the watermark.
-   * @param WaterText
-   *          The watermark text.
-   * @param opacity
-   *          The opacity of the watermark.
-   *          <p>
-   *          The value should be between {@code 0.0f} and {@code 1.0f}, where
-   *          {@code 0.0f} is completely transparent, and {@code 1.0f} is
-   *          completely opaque.
-   */
-  public TextWatermark(Position position, WaterText WaterText, float opacity) {
-    if (position == null) {
-      throw new NullPointerException("Position is null.");
-    }
-    if (WaterText == null) {
-      throw new NullPointerException("Watermark Text is null.");
-    }
-    if (opacity > 1.0f || opacity < 0.0f) {
-      throw new IllegalArgumentException("Opacity is out of range of "
-          + "between 0.0f and 1.0f.");
-    }
-
-    this.position = position;
-    this.WaterText = WaterText;
-    this.opacity = opacity;
-  }
-
-  public BufferedImage apply(BufferedImage img) {
-    int width = img.getWidth();
-    int height = img.getHeight();
-    int type = img.getType();
-
-    BufferedImage imgWithWatermark = new BufferedImageBuilder(width, height,
-        type).build();
-
-    Graphics2D g = imgWithWatermark.createGraphics();
-    int watermarkWidth = WaterText.getHeight();
-    int watermarkHeight = WaterText.getWidth();
-
-    Point p = position.calculate(width, height, watermarkWidth,
-        watermarkHeight, position.getinsetLeft(), position.getinsetRight(),
-        position.getinsetTop(), position.getinsetBottom());
-    // 设置颜色 Color.red
-    g.setColor(WaterText.getColor());
-
-    // 设置 Font new Font("宋体", Font.BOLD, 30)
-    g.setFont(WaterText.getFont());
-
-    g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, opacity));
-
-    // 第一参数->设置的内容，后面两个参数->文字在图片上的坐标位置(x,y) .
-    g.drawString(WaterText.getContent(), p.x, p.y);
-
-    g.dispose();
-
-    return imgWithWatermark;
-  }
+		double yRatio = p.y / (double)img.getHeight();
+		int yOffset = (int)((1.0 - yRatio) * captionHeight);
+		
+		g.drawString(caption, p.x, p.y + yOffset);
+		
+		g.dispose();
+		
+		return newImage;
+	}
 }
